0d101d146afe83721aad3a8190ff592ba84f7505
Jigyashusingh <105666945+Jigyashusingh@users.noreply.github.com>
2024-02-05 01:50:29 +0530

    Create stl.cpp

---
+++ stl.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+    
+    vector<int >v(4,20);
+    vector<int >v2 = v;
+    //vector<int >v2(v);
+    for(int i=0;i<v2.size();i++){
+        cout<<v[i]<<endl;
+    }
+    
+    
+    // cout<<v[2];
+    v.push_back(3);
+    cout<<v.size()<<endl;
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+    
+    
+    
+    vector<pair<int,int>>vec;
+    vec.push_back({1,2});
+    vec.emplace_back(1,2);
+    cout<<vec[0].first<<vec[0].second;
+    
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    
+  
+    vector<int >v;
+    int n;
+    int sum=0;
+    cout<<"enter the size of the array";
+    cin>>n;
+    cout<<"enter the elements of an aray";
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+    
+    cout<<"sum of all elements of an array";
+    for(int i=0;i<n;i++){
+        
+        sum+=v[i];
+    }
+    cout<<sum;
+   
+    
+    cout<<v.size()<<endl;
+    cout<<v.capacity();
+
+} 
