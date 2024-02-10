540d243de89088c67163948e9864edf4d661c059
Jigyashusingh <105666945+Jigyashusingh@users.noreply.github.com>
2024-02-10 19:16:16 +0530

    Update stl.cpp

--- stl.cpp
+++ stl.cpp
@@ -134,9 +134,20 @@ int main(){
     //     cout<<i<<endl;
     // }
 
-
-
     
+    //   permutataion
+
+    //     string s="123";
+    //     sort(s.begin(),s.end());
+        
+    //     do{
+    //         cout<<s<<endl;
+    //     }
+    //     while(next_permutation(s.begin(),s.end()));
+        
+    // }
+    
+        
 
 
 } 
