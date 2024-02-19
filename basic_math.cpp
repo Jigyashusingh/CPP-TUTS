2350361606555f7adc8ea4f00a75fa6583b1919d
Jigyashusingh <105666945+Jigyashusingh@users.noreply.github.com>
2024-02-20 00:23:51 +0530

    Update basic_math.cpp

--- basic_math.cpp
+++ basic_math.cpp
@@ -16,4 +16,20 @@
             
     //     }
     //     cout<<rev;
-
+    
+    // int n,rev=0 ;
+    //     cin>>n;
+    //     int m =n;
+    //     while(n!=0){
+    //         int l=n%10;
+    //         rev=(rev*10)+l;
+    //         n/=10;
+            
+    //     }
+    //     cout<<rev<<endl;
+    //     if(m==rev){
+    //         cout<<"true";
+    //     }
+    //     else{
+    //         cout<<"false";
+    //     }
