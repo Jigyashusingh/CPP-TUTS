89680c282ceaa8f6c4482686c5d4ba7529e61acc
Jigyashusingh <105666945+Jigyashusingh@users.noreply.github.com>
2024-02-24 14:35:52 +0530

    Update basic_math.cpp

--- basic_math.cpp
+++ basic_math.cpp
@@ -47,3 +47,21 @@
     	
     // }
 
+    // int prime(int n){
+    //     int count=0;
+    //     for(int i=1;i<=sqrt(n);i++){
+    //         if(n%i==0){
+    //             count+=1;
+    //             if(n/i!=i){
+    //                 count+=1;
+    //             }
+    //         }
+    //     }
+    //     if(count==2 ){
+    //         cout<<"is prime";
+    //     }
+    //     else{
+    //         cout<<" is not prime";
+    //     }
+    //     return 0;
+    // }
