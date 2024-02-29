2b65293d3b5ebc6fab47cd627e3e4c42e941d5e5
Jigyashu Singh <105666945+Jigyashusingh@users.noreply.github.com>
2024-02-29 15:00:23 +0530

    Update Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -71,3 +71,25 @@
       //     cin>>n;
       // print(1,n);
       // }
+
+      
+      
+      // #define nl cout<<endl;
+      
+      // void print(int i,int n){
+          
+      //     if(i>n){
+      //         return;
+      //     }
+      //     else{ 
+      //         cout<<i;nl
+      //         print(i+1,n);
+      //     }
+      
+      // }
+      
+      // int main(){
+      //     int n;
+      //     cin>>n;
+      // print(1,n);
+      // }
