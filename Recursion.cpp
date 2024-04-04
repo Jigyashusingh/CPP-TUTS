9dfd09b4bfb724a55258b41c2e82320a9f4c6978
Jigyashu Singh <105666945+Jigyashusingh@users.noreply.github.com>
2024-04-05 00:51:51 +0530

    Update Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -119,3 +119,24 @@
       // }
 
 
+      // #include<bits/stdc++.h>
+      // using namespace std;
+            
+      // int summation(int n){
+      //     if(n==0)
+      //     {
+      //         return 0;
+      //     }
+      //     else{
+      //         return n+summation(n-1);
+      //     }
+         
+      // }
+            
+      //  int main(){
+      //         int n;
+      //         cin>>n;
+      //         cout<<summation(n);
+            
+      //       }
+
